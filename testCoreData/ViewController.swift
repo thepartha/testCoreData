3174a3a17ceb4c3519da0c8719cfafa8be932ad6
Partha <the.saintneo@gmail.com>
2020-06-13 10:08:21 -0400

    testCoreData

---
+++ testCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testCoreData
+//
+//  Created by Partha Sarathy on 6/11/20.
+//  Copyright © 2020 Partha Sarathy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
